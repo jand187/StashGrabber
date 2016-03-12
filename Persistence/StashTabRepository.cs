5d59ffff9262dbd1cd864cd560273081dc6023e5
Jan Daniel Anderse <jand187@gmail.com>
2016-03-12 18:10:32 +0100

    Added persistence proj.

---
+++ Persistence/StashTabRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SQLite;
+
+namespace Persistence
+{
+	public class StashTabRepository
+	{
+	}
+
+	public class Database
+	{
+		public string DbFile
+		{
+			get { return  @"C:\Users\JanDaniel\Documents\GitHub\StashGrabber\SimpleDb.sqlite"; }
+		}
+
+		public void CreateDatbase()
+		{
+			var conn = new SQLiteConnection("Data Source=" + DbFile);
+			using (conn)
+			{
+				conn.Open();
+				var cmd = new SQLiteCommand(
+					@"create table ItemsRaw (
+						 ID								integer identity primary key AUTOINCREMENT,
+						 Name							varchar(100) not null,
+						 ExternalId						varchar(100) not null,
+						 Updated						datetime not null
+					)", conn);
+
+				cmd.ExecuteNonQuery();
+			}
+		}
+	}
+}
