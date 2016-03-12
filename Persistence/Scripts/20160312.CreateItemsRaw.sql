fd26adf45e8fee6c031fe49fc90063500e32c41a
Jan Daniel Anderse <jand187@gmail.com>
2016-03-12 23:04:23 +0100

    Added script for table ItemsRaw.

---
+++ Persistence/Scripts/20160312.CreateItemsRaw.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[ItemsRaw] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [Name]        NCHAR (100)  NULL,
+    [ExternalId ] NCHAR (100)  NOT NULL,
+    [ItemLevel]   NCHAR (10)   NULL,
+    [Icon]        NCHAR (1024) NULL,
+    [TypeLine]    NCHAR (100)  NULL,
+    [Corrupted]   BIT          NULL,
+    [Identified]  BIT          NULL,
+    [Note]        NCHAR (100)  NULL,
+    [InventoryId] NCHAR (20)   NULL,
+    [LastUpdated] DATETIME     NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([ExternalId ] ASC)
+);
+
