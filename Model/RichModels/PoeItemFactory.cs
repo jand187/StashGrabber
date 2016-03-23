da21ac9b9440a38c434cffcd7b51cd674b4af26d
Jan Daniel Anderse <jand187@gmail.com>
2016-03-23 08:59:59 +0100

    ...

---
+++ Model/RichModels/PoeItemFactory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Model.SimpleModels;
+
+namespace Model.RichModels
+{
+	public class PoeItemFactory
+	{
+		public PoeItem Create(StashItem inputItem)
+		{
+			return new PoeItem
+			{
+				Name = inputItem.Name,
+				ExternalId = inputItem.ExternalId,
+				ItemLevel = Convert.ToInt32(inputItem.ItemLevel),
+				Icon = inputItem.Icon,
+				TypeLine = inputItem.TypeLine,
+				Corrupted = Convert.ToBoolean(inputItem.Corrupted),
+				Identified = Convert.ToBoolean(inputItem.Identified),
+				Note = inputItem.Note,
+				InventoryId = inputItem.InventoryId,
+				NumberOfSockets = inputItem.Sockets.Count(),
+				NumberOfLinkedSockets = GetLinkedSockets(inputItem.Sockets),
+				ExplicitMods= new List<PoeMod>(),
+			};
+		}
+
+		private int GetLinkedSockets(IEnumerable<ItemSocket> sockets)
+		{
+			var links = 0;
+
+			for (var groupId = 0; groupId < 6; groupId++)
+			{
+				sockets.Where(s => s.group == groupId)
+
+
+			}
+		}
+	}
+
+	public class PoeItem
+	{
+		public string Name { get; set; }
+
+		public string ExternalId { get; set; }
+
+		public int ItemLevel { get; set; }
+
+		public string Icon { get; set; }
+
+		public string TypeLine { get; set; }
+
+		public bool Corrupted { get; set; }
+
+		public bool Identified { get; set; }
+
+		public string Note { get; set; }
+
+		public string InventoryId { get; set; }
+
+		public int NumberOfSockets { get; set; }
+
+		public int NumberOfLinkedSockets { get; set; }
+
+		public IEnumerable<PoeMod> ExplicitMods { get; set; }
+	}
+
+	public class PoeMod
+	{
+	}
+}
